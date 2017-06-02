4e011df226cd25b36ee93cbe3fa77af732247647
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-06-02 09:12:30 +0300

    Работа с датой скрипт акции бронирования

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var bs = require("browser-sync").create();
 
 //тут
-bs.watch(['global-object/*.*', 'css/*.css']).on("change", bs.reload);
+bs.watch(['date/exzample/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  proxy: "js-jquery-dakett/global-object"
+  proxy: "js-jquery-dakett/date/exzample"
 });
